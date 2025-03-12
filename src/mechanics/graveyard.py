8aae42062ebef965019f75e4e85380980a92da57
snoozleEmily <emilyalvimicm@gmail.com>
2025-03-12 19:49:20 -0300

    Create graveyard.py

---
+++ src/mechanics/graveyard.py
@@ -0,0 +1,12 @@
+from character import MainCharacter
+
+
+# Salvar aqui as instâncias de personagens que morreram
+# Ter módulo para:
+# - Mostrar o último personagem no end game
+# - Mostrar todos os personagens no graveyard
+#   -- Mostrar a história completa de cada um. 
+#      [problemas com isso: com tempo o jogo vai ficar muito pesado. 
+#      Considerar a implementação de clear cache e 
+#      salvar os arquivos de histórico em outro lugar,
+#      como um arquivo de texto separado, por exemplo]

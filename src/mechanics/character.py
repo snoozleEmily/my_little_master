9b631fb0f0d5b7ce1e117df4b727f95fb1e6fcf3
jluizcalonio <maiacalonio@gmail.com>
2025-03-15 22:14:38 -0300

    Updated character.py

--- src/mechanics/character.py
+++ src/mechanics/character.py
@@ -1,6 +1,7 @@
 # import quantum_letter_matrix as qlm
 import os
 import event
+import itertools
 
 # Esse log / histórico poderia, por exemplo, gravar o que ele fez no evento X e a consequência daquilo,
 # assim fica uma espécie de história da vida dele depois que ele morre,
@@ -32,19 +33,43 @@ class MainCharacter:
         self.base_dir = os.path.dirname(
             os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         )
+
+
         self.char_dir = os.path.join(self.base_dir, "data", "characters", char_name)
-        self.file_path = os.path.join(self.char_dir, f"{self.char_name}.txt")
+
+        # Generates a new char_dir if char_dir already exists
+        self.char_dir = self.generate_dir_name(self.char_dir)
 
         os.makedirs(self.char_dir, exist_ok=True)
+
+        self.file_path = os.path.join(self.char_dir, f"{self.char_name}.txt")
 
         # with open(f"{self.char_dir}\\{char_name}.txt", "w") as char_hist_file:
         if not os.path.exists:
             with open(self.file_path, "w") as char_hist_file:
                 char_hist_file.write(f"{char_name}'s Life History\n")
 
-    def check_status(
-        self,
-    ):  # Checks if all attributes > 0, else it's false and char is dead
+    def generate_dir_name(self, base_dir_path):
+        """
+        March 15th, 2025 - Added a function to generate a new char_dir if char_dir already exists
+
+        Arg:
+            base_dir_path: The self.char_dir path you want to check.
+
+        Returns:
+            if char_dir doesn't exist: returns the path set in self.char_dir without changes
+
+            if char_dir already exists: returns the path with modified name using a counter.
+                Example: If "TestChar" already exists and you want to create another "TestChar" dir,
+                the function will name the new dir as "TestChar(1)".
+                If you try to create yet another TestChar dir, this time it will be named "TestChar(2)" and so on...
+        """
+        if not os.path.exists(base_dir_path):
+            return base_dir_path
+        else:
+            return next(f"{base_dir_path}({i})" for i in itertools.count(1) if not os.path.exists(f"{base_dir_path}({i})"))
+
+    def check_status(self):  # Checks if all attributes > 0, else it's false and char is dead
         self.is_alive = all(value > 0 for value in self.attributes.values())
 
         if not self.is_alive:

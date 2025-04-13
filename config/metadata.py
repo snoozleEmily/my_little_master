249a385851d0878d33d3e811a00ac8dbf2eaef8e
snoozleEmily <emilyalvimicm@gmail.com>
2025-04-13 17:21:01 -0300

    Update metadata.py

--- config/metadata.py
+++ config/metadata.py
@@ -5,5 +5,16 @@ from __url__ import __url__
 NAME = "My Little Master"
 DESCRIPTION = "A narrative-driven game where player choices shape the story and influence the world"
 LICENSE = "GPLv3"
-KEYWORDS = [""]
+KEYWORDS = [
+    "game",
+    "procedural-generation",
+    "narrative",
+    "rpg",
+    "jinja2",
+    "dynamic-storytelling",
+    "choice-based",
+    "text-based",
+    "localization",
+    "open-source",
+]
 PROJECT_URLS = __url__

5f1bf27403f9385cb83376790ccaaa0247b4ced1
snoozleEmily <emilyalvimicm@gmail.com>
2025-04-13 17:20:57 -0300

    Update classifiers.py

--- config/classifiers.py
+++ config/classifiers.py
@@ -1,3 +1,17 @@
 CLASSIFIERS = [
-    # Add the classifiers 
-]
+    "Development Status :: 4 - Beta",  # Since it's WIP but has an MVP
+    "Intended Audience :: End Users/Desktop",
+    "License :: OSI Approved :: GNU General Public License (GPL)",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Operating System :: OS Independent",
+    "Topic :: Games/Entertainment",
+    "Topic :: Games/Entertainment :: Role-Playing",
+    "Natural Language :: English",
+    "Natural Language :: Portuguese", # Planned support
+    "Natural Language :: Spanish",  # Planned support
+],
